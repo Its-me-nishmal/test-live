39a5cd002513ff4672bfa89e59ee0aa67c6baad5
Muhammad Nishmal P <138182452+Its-me-nishmal@users.noreply.github.com>
2024-10-15 08:23:07 +0000

    ok

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const http = require('http');
 const GOOGLE_SHEET_URL = 'https://script.googleusercontent.com/macros/echo?user_content_key=jrY6qgcIP0HclGr9W22qvj5h6WNkSG2yZRoIqKWQK9XMRTSBwYotjMZjavLa99F0QwDPZKvjjDZl9QU5FT0RP42aovy3fRUUm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnC_lH6Xes5sj60JYhGEpjf2RIE1hP9cK6jI0Ln1fsVsK0LcP46IkpFL8F9V7EmWWl2Qm3wh0R6cbKFXj2yb-_wPsOF67hWI8cNz9Jw9Md8uu&lib=MJrc9AgLu8ITr5HB7zZTL_mIm8UQkONTv'; // Your Google Apps Script URL
 const OVERLAY_FILE = path.join(__dirname, 'overlay.txt'); // Text file to store the overlay text
 const MP3_FILE = path.join(__dirname, 'sample.mp3'); // Path to the MP3 audio file
+const BACKGROUND_IMAGE = path.join(__dirname, 'bg.png'); // Path to the static black background image
 const STREAM_KEY = 'su9z-j176-664y-47wb-1zq4'; // Your YouTube stream key
 const PORT = process.env.PORT || 3000; // HTTP server port
 
@@ -32,10 +33,10 @@ async function fetchYouTubeData() {
   }
 }
 
-// Start FFmpeg to Stream with Static Background and Overlay
+// Start FFmpeg to Stream with Static Background Image and Overlay
 function startFFmpeg() {
   ffmpeg()
-    .input('color=c=black:size=1280x720') // Generate a static black background with 1280x720 resolution
+    .input(BACKGROUND_IMAGE) // Use the static black background image as the video input
     .input(MP3_FILE) // Audio input (MP3 file)
     .complexFilter([
       // Overlay text data (reload the overlay file every second)
